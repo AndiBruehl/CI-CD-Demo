6339b1a0986ba76683cfc2fefb4b50b00220d5ce
AndiBruehl <127749678+AndiBruehl@users.noreply.github.com>
2023-11-21 15:17:57 +0100

    ha139

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #     version = "~> 5.0"
+  #   }
+  # }
 
   backend "s3" {
     bucket = "{{ secrets.TFSTATE_BUCKET_NAME }}"
