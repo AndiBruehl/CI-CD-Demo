1ed7f9dd13fe195c7136bf412d0a05fa9dbe6272
AndiBruehl <127749678+AndiBruehl@users.noreply.github.com>
2023-11-21 14:53:51 +0100

    ha139

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,4 @@
 module "s3-website" {
   source = "./modules/s3-website"
-  bucket_name = var.bucket_name
+  bucket_name = "my-website-bucket-234808sfd"
 }
-
-
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,13 +2,8 @@ variable "region" {
   type    = string
   default = "eu-central-1"
 }
+
 variable "aws_profile" {
   type    = string
-  default = "360425496229_Student"
-}
-
-
-variable "bucket_name" {
- description = "Name of the S3 bucket to create"
- type        = string
+  default = "techstarter"
 }
